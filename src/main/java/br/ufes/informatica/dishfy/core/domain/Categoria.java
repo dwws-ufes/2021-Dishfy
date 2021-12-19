0a233466cd824f680af8359c8f780cede720a519
Wagner Porto <wagner741@gmail.com>
2021-12-19 14:53:01 -0300

    adição classe categoria

---
+++ src/main/java/br/ufes/informatica/dishfy/core/domain/Categoria.java
@@ -0,0 +1,39 @@
+package br.ufes.informatica.dishfy.core.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categoria {
+    private int id;
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private String nome;
+
+    public Categoria(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+
+    
+}
