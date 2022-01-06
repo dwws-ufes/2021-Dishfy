6b7d40c7a7fe723bbcc7f7546fc0a51ef60bff99
Wagner Porto <wagner741@gmail.com>
2022-01-06 11:18:54 -0300

    Update ReceitaImpl

--- src/main/java/br/ufes/informatica/dishfy/core/persistence/ReceitaImpl.java
+++ src/main/java/br/ufes/informatica/dishfy/core/persistence/ReceitaImpl.java
@@ -1,12 +1,7 @@
 package br.ufes.informatica.dishfy.core.persistence;
 
 
-import java.util.List;
-
 import javax.persistence.EntityManager;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-
 
 import br.ufes.informatica.dishfy.core.domain.Receita;
 public class ReceitaImpl {
@@ -33,6 +28,19 @@ public class ReceitaImpl {
             return this.updateReceita(receita);
         }
         return this.saveReceita(receita);
+
+    }
+    public Receita getReceita(Receita receita) {
+        return em.find(Receita.class, receita);
+
+    }
+
+    public List<Receita> getAllReceita() {
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<Receita> criteriaQuery = criteriaBuilder.createQuery(Receita.class);
+        criteriaQuery.select(criteriaQuery.from(Receita.class));
+        List<Receita> usuarios = em.createQuery(criteriaQuery).getResultList();
+        return usuarios;
 
     }
 
