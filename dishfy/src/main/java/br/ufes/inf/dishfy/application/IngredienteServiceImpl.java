5ad465a82f5de4f8bda876875a97f5812e9f3010
Gabriel Ferrari <gabrielferrari0001@gmail.com>
2022-01-25 12:50:21 -0300

    ingredientes

--- dishfy/src/main/java/br/ufes/inf/dishfy/application/IngredienteServiceImpl.java
+++ dishfy/src/main/java/br/ufes/inf/dishfy/application/IngredienteServiceImpl.java
@@ -1,5 +1,6 @@
 package br.ufes.inf.dishfy.application;
 
+import br.ufes.inf.dishfy.domain.Ingrediente;
 import br.ufes.inf.dishfy.persistence.IngredienteDao;
 import jakarta.ejb.EJB;
 import jakarta.ejb.Stateless;
@@ -8,4 +9,10 @@ import jakarta.ejb.Stateless;
 public class IngredienteServiceImpl implements IngredienteService{
     @EJB
     private IngredienteDao ingredienteDao;
+
+    public Ingrediente getIngrediente(String nome) {
+        return ingredienteDao.getIngredientesByName(nome);
+    }
+
+
 }
--- dishfy/src/main/java/br/ufes/inf/dishfy/control/IngredienteController.java
+++ dishfy/src/main/java/br/ufes/inf/dishfy/control/IngredienteController.java
@@ -2,6 +2,18 @@ package br.ufes.inf.dishfy.control;
 
 import java.io.Serializable;
 
+import br.ufes.inf.dishfy.application.IngredienteService;
+import br.ufes.inf.dishfy.domain.Ingrediente;
+import jakarta.ejb.EJB;
+
 public class IngredienteController implements Serializable {
+
+    @EJB
+    private IngredienteService ingredienteService;
+
     private String nome;
+
+    public Ingrediente consultaIngrediente(){
+        return ingredienteService.getIngrediente(nome);
+    }
 }
